385d150d8a09825291e30b2bcb8c8ba333fd95f2
YY T <changiyy07@gmail.com>
2024-04-14 21:25:43 +0800

    created user seeds to test cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "open-uri"
+
+puts "resetting data base..."
+Challenge.destroy_all
+User.destroy_all
+
+puts "creating entries..."
+
+# create users
+
+user1 = User.create!(
+  email: "user1@email.com",
+  password: "password",
+  first_name: "Melissa",
+  last_name: "Li",
+  admin: false
+)
+
+file = URI.open("https://thispersondoesnotexist.com/")
+user1.avatar.attach(io: file, filename: "avatar1.jpeg", content_type: "image/jpeg")
+user1.save
+
+user2 = User.create!(
+  email: "user2@email.com",
+  password: "password",
+  first_name: "Melvin",
+  last_name: "Tan",
+  admin: true
+)
+
+file = URI.open("https://thispersondoesnotexist.com/")
+user2.avatar.attach(io: file, filename: "avatar2.jpeg", content_type: "image/jpeg")
+user2.save
+
+puts "seeding entries done!"
