54f61bddd14abae5df252d4b4682c44b353c8dc6
YY T <changiyy07@gmail.com>
2024-04-17 20:25:21 +0800

    Fix irregular home card sizes by fixing width and height

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -4,4 +4,49 @@ class ChallengesController < ApplicationController
   def index
     @challenges = policy_scope(Challenge)
   end
+
+  def show
+    @challenge = Challenge.find(params[:id])
+    authorize @challenge
+  end
+
+  def new
+    @challenge = Challenge.new
+    authorize @challenge
+  end
+
+  def create
+    @challenge = Challenge.new(params_challenge)
+    authorize @challenge
+    @challenge.user = current_user
+    if @challenge.save
+      redirect_to challenges_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @challenge = Challenge.find(params[:id])
+    authorize @challenge
+  end
+
+  def update
+    @challenge = Challenge.find(params[:id])
+    authorize @challenge
+    @challenge.update(params_challenge)
+    redirect_to challenge_path(@challenge)
+  end
+
+  def destroy
+    @challenge = Challenge.find(params[:id])
+    authorize @challenge
+    @challenge.destroy
+  end
+
+  private
+
+  def params_challenge
+    params.require(:challenge).permit(:name, :description, :participant_criteria, :start_date, :end_date, :cover [])
+  end
 end
--- app/policies/challenge_policy.rb
+++ app/policies/challenge_policy.rb
@@ -1,5 +1,28 @@
 class ChallengePolicy < ApplicationPolicy
   class Scope < Scope
+    def show?
+      true
+    end
+
+    def new?
+      record.user.admin == true
+    end
+
+    def create?
+      record.user.admin == true
+    end
+
+    def edit?
+      record.user.admin == true
+    end
+
+    def update?
+      record.user.admin == true
+    end
+
+    def destroy?
+      record.user.admin == true
+    end
 
     def resolve
       scope.all
