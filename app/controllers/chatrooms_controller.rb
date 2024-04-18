f6ae1a50d2736037662e65c07efab584434e9727
YY T <changiyy07@gmail.com>
2024-04-18 13:01:43 +0800

    complete the creation of the differing page layouts

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,6 +2,8 @@ class ChatroomsController < ApplicationController
 
   def index
     @chatrooms = policy_scope(Chatroom)
+    @title = "Collaboration Chats"
+    render layout: "internalpage_layout"
   end
 
 end
