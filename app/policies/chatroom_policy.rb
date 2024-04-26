5fec62e75b0df264af189b29375d08f31e1dc907
YY T <changiyy07@gmail.com>
2024-04-26 14:21:32 +0800

    add comment on chatroom policy scope

--- app/policies/chatroom_policy.rb
+++ app/policies/chatroom_policy.rb
@@ -6,6 +6,7 @@ class ChatroomPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.joins(:chatroom_members).where(chatroom_members: { user: })
+      # this finds all chatrooms with members, where the member includes the user
     end
   end
 end
